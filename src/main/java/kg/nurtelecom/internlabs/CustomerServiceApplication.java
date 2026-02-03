e0e9b73170aa9d2e7594d1fc764f2dab8742c34e
N1heo <napsatarov@gmail.com>
2026-02-03 12:48:41 +0600

    first commit

---
+++ src/main/java/kg/nurtelecom/internlabs/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.nurtelecom.internlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/nurtelecom/internlabs/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.nurtelecom.internlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
