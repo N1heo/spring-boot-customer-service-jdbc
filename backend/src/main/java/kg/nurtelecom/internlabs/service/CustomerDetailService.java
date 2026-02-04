6c3c57b89e2b37b4420b280766ff68f9abb5184c
Mambetkulov <muganm320@gmail.com>
2026-02-04 21:45:45 +0600

    comment

--- backend/src/main/java/kg/nurtelecom/internlabs/service/CustomerDetailService.java
+++ backend/src/main/java/kg/nurtelecom/internlabs/service/CustomerDetailService.java
@@ -1,6 +1,8 @@
 package kg.nurtelecom.internlabs.service;
 
 import kg.nurtelecom.internlabs.model.UserPrinciple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,9 +11,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerDetailService implements UserDetailsService {
 
+
+    private static final Logger log = LoggerFactory.getLogger(CustomerDetailService.class);
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
+        // пока тестовый, реальная реализация после подключение jdbc
         return new UserPrinciple("bolo", "$2a$12$FCtID4P6GY2uEL86P12KQuYtlstORhynA6wjCWGkdVFXqXKKpgaUC");
     }
 }
