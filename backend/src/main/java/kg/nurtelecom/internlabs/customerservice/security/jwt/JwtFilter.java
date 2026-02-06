e92eebbe08ddd7ab73e1d4322774fc4f9a841b68
Mambetkulov <muganm320@gmail.com>
2026-02-06 10:38:22 +0600

    fixx

--- backend/src/main/java/kg/nurtelecom/internlabs/customerservice/security/jwt/JwtFilter.java
+++ backend/src/main/java/kg/nurtelecom/internlabs/customerservice/security/jwt/JwtFilter.java
@@ -31,6 +31,12 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain)  throws ServletException, IOException {
+        String path = request.getRequestURI();
+        if (path.equals("/login") || path.equals("/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
