83f7e7bee8a87dcaac112df70d4aecdfa2875d58
Элтуран <elturansultankulov1@gmail.com>
2026-02-08 00:47:46 +0600

    fix Profile.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,7 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {FontAwesomeIcon} from "./plugins/font-awesome";
 
+import axios from "axios";
 
 
 // axios.defaults.withCredentials = true;
