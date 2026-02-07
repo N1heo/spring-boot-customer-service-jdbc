19e2e4eef27395ebd17c4ffc8a1bc4ec3fd45a42
Элтуран <elturansultankulov1@gmail.com>
2026-02-07 21:02:42 +0600

    fix FileControllerAPI

--- backend/src/main/java/kg/nurtelecom/internlabs/customerservice/controller/FileControllerAPI.java
+++ backend/src/main/java/kg/nurtelecom/internlabs/customerservice/controller/FileControllerAPI.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/files")
+@RequestMapping("/uploads")
 public class FileControllerAPI {
 
     private final StorageService storageService;
