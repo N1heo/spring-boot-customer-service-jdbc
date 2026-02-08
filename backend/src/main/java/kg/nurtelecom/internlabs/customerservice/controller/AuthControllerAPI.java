18d336e2422fb2e6376c5af38a42efd30b2cb33c
Эржан Нурдавлетов <erzhan.nurdavletov@alatoo.edu.kg>
2026-02-08 21:11:09 +0600

    fix: add filters based on Erkin agai lesson 50 (#26)
    * fix: add filters based on Erkin agai lesson 50
    
    * add @Component above CustomAuthenticationFilter
    
    ---------
    
    Co-authored-by: Эржан Нурдавлетов <nurdavletoverjan@gmail.com>

--- backend/src/main/java/kg/nurtelecom/internlabs/customerservice/controller/AuthControllerAPI.java
+++ backend/src/main/java/kg/nurtelecom/internlabs/customerservice/controller/AuthControllerAPI.java
@@ -1,7 +1,6 @@
 package kg.nurtelecom.internlabs.customerservice.controller;
 
 import jakarta.validation.Valid;
-import kg.nurtelecom.internlabs.customerservice.payload.request.auth.LoginRequest;
 import kg.nurtelecom.internlabs.customerservice.payload.request.auth.RegisterCustomerRequest;
 import kg.nurtelecom.internlabs.customerservice.payload.response.AuthResponse;
 import kg.nurtelecom.internlabs.customerservice.service.AuthService;
@@ -19,23 +18,12 @@ public class AuthControllerAPI {
         this.authService = authService;
     }
 
-    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AuthResponse> registerJson(@RequestBody @Valid RegisterCustomerRequest request) {
-        return ResponseEntity.ok(authService.register(request, null));
-    }
-
     @PostMapping(value = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<AuthResponse> register(
             @RequestPart("data") @Valid RegisterCustomerRequest request,
             @RequestPart(value = "photo", required = false) MultipartFile photo
     ) {
         AuthResponse resp = authService.register(request, photo);
-        return ResponseEntity.ok(resp);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest req) {
-        AuthResponse resp = authService.login(req);
         return ResponseEntity.ok(resp);
     }
 }
--- backend/src/main/java/kg/nurtelecom/internlabs/customerservice/repository/AuthRepository.java
+++ backend/src/main/java/kg/nurtelecom/internlabs/customerservice/repository/AuthRepository.java
@@ -2,17 +2,12 @@ package kg.nurtelecom.internlabs.customerservice.repository;
 
 import kg.nurtelecom.internlabs.customerservice.enums.Role;
 import kg.nurtelecom.internlabs.customerservice.exception.ConflictException;
-import kg.nurtelecom.internlabs.customerservice.payload.request.auth.LoginRequest;
 import kg.nurtelecom.internlabs.customerservice.payload.request.auth.RegisterCustomerRequest;
 import kg.nurtelecom.internlabs.customerservice.payload.response.AuthResponse;
 import kg.nurtelecom.internlabs.customerservice.repository.jdbc.JdbcConnectionFactory;
-import kg.nurtelecom.internlabs.customerservice.security.UserPrinciple;
 import kg.nurtelecom.internlabs.customerservice.security.jwt.JwtService;
 import kg.nurtelecom.internlabs.customerservice.service.AuthService;
 import kg.nurtelecom.internlabs.customerservice.storage.StorageService;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,7 +22,6 @@ public class AuthRepository implements AuthService {
   private final JdbcConnectionFactory connectionFactory;
   private final AuthRepositoryJdbc authJdbc;
   private final JwtService jwtService;
-  private final AuthenticationManager authenticationManager;
   private final PasswordEncoder passwordEncoder;
   private final StorageService storage;
 
@@ -35,14 +29,12 @@ public class AuthRepository implements AuthService {
       JdbcConnectionFactory connectionFactory,
       AuthRepositoryJdbc authJdbc,
       JwtService jwtService,
-      AuthenticationManager authenticationManager,
       PasswordEncoder passwordEncoder,
       StorageService storage
   ) {
     this.connectionFactory = connectionFactory;
     this.authJdbc = authJdbc;
     this.jwtService = jwtService;
-    this.authenticationManager = authenticationManager;
     this.passwordEncoder = passwordEncoder;
     this.storage = storage;
   }
@@ -98,20 +90,4 @@ public class AuthRepository implements AuthService {
         authJdbc.normalizeEmail(request.getEmail()), role);
     return new AuthResponse(token);
   }
-
-  @Override
-  public AuthResponse login(LoginRequest req) {
-    Authentication auth = authenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(
-            authJdbc.normalizeEmail(req.getEmail()),
-            req.getPassword()
-        )
-    );
-
-    UserPrinciple user = (UserPrinciple) auth.getPrincipal();
-    String token = jwtService.generateToken(
-        user.getUsername(), user.getRole());
-    return new AuthResponse(token);
-  }
 }
-
